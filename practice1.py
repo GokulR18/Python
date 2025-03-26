b073a97c7c64ba72d6e7551c45a1a782705810b6
GokulR18 <gokulr2031@gmail.com>
2025-03-26 13:06:14 +0530

    ok

---
+++ practice1.py
@@ -0,0 +1,19 @@
+A = int(input("Enter a value :"))
+B = int(input("Enter a value :"))
+ADD = A+B
+
+print(" Sum of the values is : ",ADD)
+
+Z = int(input("Enter a value :"))
+
+Reminder= (A+B)%Z
+print("Remider of the values is :", Reminder)
+
+X = input("Enter a Input")
+print(type(X))
+
+C= A>B
+print("Is A greater than B :",C)
+print("The Average of A and B is :",(A+B)/2)
+print("The Square of B is:", B*B)
+
---
+++ practice2.py
@@ -0,0 +1,9 @@
+NAME = input("Enter your Name :")
+print("Good Morning ",NAME)
+DATE = input(" ENter a Date :")
+
+Letter = '''DEAR NAME,
+YOUR ARE SELECTED
+DATE'''
+print(Letter.replace("NAME", NAME).replace("DATE",DATE))
+print(NAME.find("A"))
